218076314cc9932c06dd5b95264bb64848b2924c
Ewald Horn <ewaldhorn@gmail.com>
2019-07-11 16:12:55 +0200

    Added activities B and C.

---
+++ app/src/main/java/za/co/nofuss/alc4_phase_1_challenge_1_ewald_horn/ActivityB.java
@@ -0,0 +1,13 @@
+package za.co.nofuss.alc4_phase_1_challenge_1_ewald_horn;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b);
+    }
+}
---
+++ app/src/main/java/za/co/nofuss/alc4_phase_1_challenge_1_ewald_horn/ActivityC.java
@@ -0,0 +1,13 @@
+package za.co.nofuss.alc4_phase_1_challenge_1_ewald_horn;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_c);
+    }
+}
